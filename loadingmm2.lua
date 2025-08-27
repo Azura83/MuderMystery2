35c56c983f3aced33da910040a18e6fb2dea8bd2
Azure <anishgiri528@gmail.com>
2025-08-28 00:08:53 +0545

    Update loadingmm2.lua

--- loadingmm2.lua
+++ loadingmm2.lua
@@ -263,9 +263,10 @@ if exec == "Delta" then
     -- Reminder every 20 seconds (still optional)
     task.spawn(function()
         while true do
-            task.wait(120)
+            task.wait(20)
             notify("Reminder: Turn OFF 'Verify Teleports' in Delta and rejoin to avoid getting kicked !")
         end
     end)
 end
+
 
